f1432fb2d901681d7b81b3b499b2561912956c5a
cattykate <ekaterina.barkova95@yandex.ru>
2016-11-12 23:42:14 +0300

    create classes A,B,C

--- src/inherit14.cpp
+++ src/inherit14.cpp
@@ -1,7 +1,68 @@
 #include <iostream>
 
-int main() {
+class A1 {
+protected:
+	int a1;
+public:
 
+	A1 (int aval) : a1(aval) {};
+};
+
+
+class B1 {
+protected:
+	int b1;
+public:
+
+	B1 (int bval) : b1(bval) {};
+};
+
+
+class B2 {
+protected:
+	int b2;
+public:
+
+	B2(int bval) : b2(bval) {};
+};
+
+class B3 {
+protected:
+	int b3;
+public:
+
+	B3(int bval) : b3(bval) {};
+};
+
+
+class C1 {
+protected:
+	int c1;
+public:
+
+	C1(int cval) : c1(cval) {};
+};
+
+
+class C2 {
+protected:
+	int c2;
+public:
+
+	C2(int cval) : c2(cval) {};
+};
+
+class C3 {
+protected:
+	int c3;
+public:
+
+	C3(int cval) : c3(cval) {};
+};
+
+
+int main() {
+	
 
 	return 0;
 }
