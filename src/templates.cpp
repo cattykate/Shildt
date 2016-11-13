679e2a9db885461211a7c141986dcc2ad5e46959
cattykate <ekaterina.barkova95@yandex.ru>
2016-11-14 01:39:44 +0300

    replace is ready

--- src/templates.cpp
+++ src/templates.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 #define arr_size 5
+ 
+// This function replace symbol x to simbol y in array: Replace(arr, x, y);.
+// Size, type of array and symbol are arbitary.
 
-template <class T> int* Replace (T* arr, T x, T y) {
+template <class T> T* Replace(T* arr, T x, T y) {
 	for (int i = 0; i < arr_size; i++) {
 		if (arr[i] == x) {
 			arr[i] = y;
@@ -13,25 +16,25 @@ template <class T> int* Replace (T* arr, T x, T y) {
 
 int main() {
 
-	int array [arr_size] = { 6, 1, 2, 3, 2 };
-	std::cout << "Current array: ";
+	int array[arr_size] = { 6, 1, 2, 3, 2 };
+	std::cout << "Array before replace: ";
 
 	for (int i = 0; i < arr_size; i++) {
 		std::cout << array[i];
 	}
 
-	Replace (array, 2, 7);
+	Replace(array, 2, 7);
 	std::cout << "\nArray after replace: ";
 	for (int i = 0; i < arr_size; i++) {
 		std::cout << array[i];
 	}
 
 
-	/*char array2[arr_size] = { 'a', 'b', 'c', 'd', 'e' };
-	std::cout << "Current array: ";
+	char array2[arr_size] = { 'a', 'b', 'c', 'd', 'e' };
+	std::cout << "\n\nArray before replace: ";
 
 	for (int i = 0; i < arr_size; i++) {
-		std::cout << array[i];
+	std::cout << array2[i];
 	}
 
 	char x = 'a';
@@ -39,8 +42,11 @@ int main() {
 	Replace(array2, x, y);
 	std::cout << "\nArray after replace: ";
 	for (int i = 0; i < arr_size; i++) {
-		std::cout << array[i];
-	}*/
+	std::cout << array2[i];
+	}
 
 	return 0;
 }
+
+
+
