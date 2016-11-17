0a47e699518caf73686b2ab66133b040a823c319
cattykate <ekaterina.barkova95@yandex.ru>
2016-11-18 01:46:32 +0300

    add overload operator> for complex numbers

--- src/classComplexOverload.cpp
+++ src/classComplexOverload.cpp
@@ -1,16 +1,25 @@
+///////////////////////////////
+//---OVERLOAD OF OPERATIONS----
+//-------CLASS COMPLEX--------
+//-------TASKS 2, 18----------
+///////////////////////////////
 #include <iostream>
+//#include <cmath>
 
 class Complex {
 	double real;
 	double image;
+	double module;
 public:
 	//Complex() { real = image = 0; }
-	Complex() : real(0), image(0) {};
-	Complex(double re, double im) : real(re), image(im) {};
+	Complex() : real(0), image(0) {}
+	Complex(double re, double im) { real = re; image = im; getModule();}
 	Complex operator+(Complex op2);
 	Complex operator+(double op2);
+	bool operator>(Complex op2);
 
 	void show();
+	void getModule();
 };
 
 // ovearload operator+ for Complex numbers
@@ -21,6 +30,7 @@ Complex Complex::operator+(Complex op2) {
 	tmp.image = image + op2.image;
 	return tmp; // return new object, arguments didn't change
 }
+
 //overload operator+ for real numbers
 Complex Complex::operator+(double op2) {
 	Complex tmp;
@@ -30,9 +40,23 @@ Complex Complex::operator+(double op2) {
 	return tmp;
 }
 
+// overload operator> for complex numbers
+bool Complex::operator>(Complex op2) {
+	if (module > op2.module) return true;
+	if (module == op2.module) {
+		if (real > op2.real) return true;
+		else return false;
+	}
+	if (module < op2.module) return false;
+}
+
 void Complex::show() {
 	std::cout << "re = " << real << std::endl;
 	std::cout << "im = " << image << "\n\n";
+}
+
+void Complex::getModule() {
+	module = sqrt(real*real + image*image);
 }
 
 int main() {
@@ -42,10 +66,10 @@ int main() {
 
 	a.show();
 	a = b + 2.0;
-
 	a.show();
 	b.show();
 	c.show();
+
 
 	return 0;
 }
