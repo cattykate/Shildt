df0d1c5627c169f20082940b137adc3ac5ebf176
cattykate <ekaterina.barkova95@yandex.ru>
2016-11-22 23:31:26 +0300

    add function get mod

--- src/classVector.cpp
+++ src/classVector.cpp
@@ -8,26 +8,55 @@
 //----------TASK 4------------
 ///////////////////////////////
 #include <iostream>
+#include <vector>
+#include <cmath>
+
 
 class Vector {
+	//std::vector <double> data;
+	double *data;
 	int size;
-	double *array;
-public:
-	Vector(int n); 
+public:	
+	Vector(int n, double *arr);
+	double getMod();
 
+	double operator [] (int a) {
+		return data[a];
+	}
+	
 };
 
-Vector::Vector(int n) {
-	size = n;
-	array = new double[n]; // allocate memory for n numbers 
+double Vector::getMod() {
+	double tmp = 0.0;
+	for (int i = 0; i < size; i++) {
+		tmp = tmp + data[i] * data[i];
+	}
+	return sqrt(tmp);
+}
 
-	for (int i = 0; i < n; i++) {
-		array[i] = 0;
+Vector::Vector(int n, double *arr) {
+	/*for (int i = 0; i < size; i++) {
+		data.push_back(0);
+	}*/
+	size = n;
+	data = new double[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		data[i] = arr[i];
 	}
 }
 
-
 int main() {
+	double a[] = { 1,3,4 };
+	Vector va(3, a);
+
+	double b[] = { 1,1,1 };
+	Vector vb(3, b);
+
+
 	
+
+
 	return 0; 
 }
