798949e397c836ea791791f3131c8805f18dd6bf
cattykate <ekaterina.barkova95@yandex.ru>
2016-11-24 00:51:29 +0300

    overload operator + for real numbers

---
+++ src/classVectorWhithoutPointers.cpp
@@ -0,0 +1,56 @@
+///////////////////////////////
+//---OVERLOAD OF OPERATIONS---
+//-----TASKS 4, 12, 20-------
+//------INPUT/OUTPUT----------
+//----------TASK 4------------
+///////////////////////////////
+#include <iostream>
+#include <vector>
+
+class Vector {
+private:
+	int size;
+public:
+	std::vector<double> vec;
+	Vector(int n);
+	
+
+	Vector operator+(double num);
+};
+
+Vector::Vector(int n) {
+	size = n;
+	for (int i = 0; i < size; i++) {
+		vec.push_back(0);
+	}
+}
+Vector Vector::operator+(double num) {
+
+	for (int i = 0; i < size; i++) {
+		vec[i] = vec[i] + num;
+	}
+	return *this;
+}
+
+
+int main() {
+	Vector va(3);
+	Vector vb(3);
+
+	va.vec[0] = 1;
+	va.vec[1] = 1;
+	va.vec[2] = 1;
+	
+
+	vb.vec[0] = 1;
+	vb.vec[1] = 1;
+	vb.vec[2] = 1;
+	
+
+	Vector vc(3);
+	double cnum = 7.0;
+	vc = va + cnum;
+
+
+	return 0;
+}
