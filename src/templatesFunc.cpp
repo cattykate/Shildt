2ea6aaadb2edc39fde446b53a71e8970d9058048
cattykate <ekaterina.barkova95@yandex.ru>
2016-11-27 04:08:02 +0300

    change

--- src/templatesFunc.cpp
+++ src/templatesFunc.cpp
@@ -3,7 +3,7 @@
 //---TASK 7-------------------
 //////////////////////////////
 #include <iostream>
-#define SIZE 4
+#define SIZE 9
 
 // Function delete all entries of x in array: Remove (arr, x)
 // Size, type of array and symbol are arbitary
@@ -15,17 +15,20 @@ template <typename T> T* Remove(T* arr, T x) {
 	for (int i = 0; i < num; i++) {
 		if (arr[i] != x) continue;
 		else {
-			for (int j = i; i < num; i++) {
+			for (int j = i; j < num; j++) {
 				arr[j] = arr[j + 1];
 			}
-			--num;
 		}
+		--num;
+		i = 0;
 	}
 	return arr;
 }
 
+
 int main() {
-	int arr[SIZE] = { 6, 1, 2, 3 };
+	int arr[SIZE] = { 6, 1, 2, 3, 2, 5, 7, 2, 4 };
 	Remove(arr, 2);
+	
 	return 0;
 }
