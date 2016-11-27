38f92368fe405033de5da05f9fd23bc24f6f2d6b
cattykate <ekaterina.barkova95@yandex.ru>
2016-11-27 19:21:17 +0300

    add function detection palindroms

--- src/templatesFunc.cpp
+++ src/templatesFunc.cpp
@@ -3,10 +3,14 @@
 //---TASK 7-------------------
 //////////////////////////////
 #include <iostream>
+#include <vector>
 
-// Function delete all entries of x in array: Remove (arr, x)
+////////////////////////////////////////////
+// -------------------TASK 7----------------
+// Function delete all entries of x in array: 
+// --------------Remove (arr, x)------------
 // Size, type of array and symbol are arbitary
-
+////////////////////////////////////////////
 template <typename T> T* Remove(int size, T* arr, T x) {
 
 	for (int i = 0; i < size; i++) {
@@ -14,36 +18,68 @@ template <typename T> T* Remove(int size, T* arr, T x) {
 		else {
 			for (int j = i; j < size; j++) {
 				arr[j] = arr[j + 1];
+
 			}
 		}
-		--size;
 		i = 0;
 	}
 	return arr;
 }
 
+////////////////////////////////////////////////////
+// -------------------TASK 12----------------
+//function checks whether the string is a palindrome
+////////////////////////////////////////////////////
+
+
+template <typename T> void Palindrom(int size, T* arr) {
+	bool fl = true;
+	int middle = 0;
+
+	if (size % 2 == 0) middle = ((size / 2)+1);
+	else middle = (size / 2);
+
+	for (int i = 0, j = (size - 1) ; i < middle; i++, j--) {
+		if (arr[i] != arr[j])
+			fl = false;
+		else {
+			i++;
+			j--;
+		}
+	}
+
+	if (fl == false) {
+		std::cout << "\nString is not a palindrom!" << std::endl;
+	}
+
+	else std::cout << "\nString is a palindrom!" << std::endl;
+}
 
 int main() {
-
+	
 	int size;
 	int * arr;
-	size = 9;
+	size = 7;
 	arr = new int[size];
-	arr[0] = 6;
-	arr[1] = 1;
-	arr[2] = 2;
-	arr[3] = 3;
-	arr[4] = 2;
-	arr[5] = 1;
-	arr[6] = 9;
-	arr[7] = 8;
-	arr[9] = 3;
 
-	Remove(size, arr, 2);
+	arr[0] = 1;
+	arr[1] = 2;
+	arr[2] = 3;
+	arr[3] = 4;
+	arr[4] = 3;
+	arr[5] = 2;
+	arr[6] = 1;
+//	arr[7] = 1;
 
-	for (int i = 0; i < size; i++) {
-		std::cout << arr[i] << " ";
-	}
-	
+
+	//Remove(size, arr, 2);
+
+	//for (int i = 0; i < size; i++) {
+	//	std::cout << arr[i] << " ";
+	//}
+
+	Palindrom(size, arr);
+
 	return 0;
+
 }
