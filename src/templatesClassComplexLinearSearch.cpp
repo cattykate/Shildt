2b5e40e4d3a86d3ed348b58c92cbdccf1901a7e8
cattykate <ekaterina.barkova95@yandex.ru>
2016-12-02 19:39:44 +0300

    create file

---
+++ src/templatesClassComplexLinearSearch.cpp
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////
+// --------- TEMPLATES OF CLASSES--------
+// --------- TASK 4 ---------------------
+// #1 template of the list
+// adding - only at the end
+// removal - only at the end
+// indexing - no
+// constructors, destructors +
+// #2 put the objects of type Complex numbers into list
+// realize Linerial Search
+////////////////////////////////////////////////////////
+#include <iostream>
+
+int main() {
+
+
+	return 0;
+}
